91e6a19a2078f00f61185d2c113adad1fb7c9663
Henry Predmore <henrypredmore@seattleacademy.orggit config --global user.name Henry>
2025-02-12 12:23:33 -0800

    Co-authored-by: eli-silk <eli-silk@users.noreply.github.com>

--- Quiz Website/app.js
+++ Quiz Website/app.js
@@ -2,86 +2,87 @@
 'use strict';
 
 function resetResult() {
-    document.getElementById("FishResult").style.display = "none";
-    document.getElementById("PlanktonResult").style.display = "none";
-    document.getElementById("HumanResult").style.display = "none";
-    document.getElementById("SpiderResult").style.display = "none";
+    document.getElementById("milkresult").style.display = "none";
+    document.getElementById("saladresult").style.display = "none";
+    document.getElementById("icecreamresult").style.display = "none";
+    document.getElementById("frenchfryresult").style.display = "none";
+    document.getElementById("frostedflakesresult").style.display = "none";
 }
 function seeResult() {
-    var Fish = 0;
-    var Plankton = 0;
-    var Nudibranch = 0;
-    var Human = 0;
-    var Spider = 0;
+    var milk = 0;
+    var salad = 0;
+    var icecream = 0;
+    var frenchfry = 0;
+    var frostedflakes = 0;
 
-    if (document.getElementById("8Eyes").checked) {
-        Spider += 1;
-    } else if (document.getElementById("2Eyes").checked) {
-        Human += 1;
-        Fish += 1;
-        Nudibranch += 1;
+    if (document.getElementById("redcolor").checked) {
+        frenchfry += 1;
+    } else if (document.getElementById("bluecolor").checked) {
+        milk += 1;
+        icecream += 1;
+        frostedflakes += 1;
     } else {
-        // 1, 2, 3 or more depending on the species
-        Plankton += 1;
+       
+        salad += 1;
     }
 
-    if (document.getElementById("Walk").checked) {
-        Human += 1;
-        Spider += 1;
-        Nudibranch += 1;
-    } else if (document.getElementById("Swim").checked) {
-        Fish += 1;
-        Plankton += 1;
-        Nudibranch += 1;
+    if (document.getElementById("saltyflavor").checked) {
+        frenchfry += 1;
+    } else if (document.getElementById("sweetflavor").checked) {
+        frostedflakes += 1;
+        icecream  += 1;
+    } else if (document.getElementById("sourflavor").checked) {
+        salad  += 1;
+        milk  += 1;
     } else {
-        // Swim upside down
-        Nudibranch += 1;
+        
     }
 
-    if (document.getElementById("Yes").checked) {
-        Human += 1;
-        Spider += 1;
-    } else if (document.getElementById("No").checked) {
-        Fish += 1;
-        Plankton += 1;
-        Nudibranch += 1;
+    if (document.getElementById("yesfridge").checked) {
+        milk += 1;
+        icecream += 1;
+        salad += 1;
+    } else if (document.getElementById("nofridge").checked) {
+        frostedflakes += 1;
+
+        frenchfry += 1;
     }
 
-    if (document.getElementById("Fight").checked) {
-        Spider += 1;
-    } else if (document.getElementById("Run").checked) {
-        Fish += 1;
-        Plankton += 1;
-    } else if (document.getElementById("Secrete").checked) {
-        Nudibranch += 1;
+    if (document.getElementById("ketchuptopping").checked) {
+        frenchfry += 1;
+    } else if (document.getElementById("mustardtopping").checked) {
+        icecream += 1;
+        frostedflakes += 1;
+    } else if (document.getElementById("ranchtopping").checked) {
+        salad += 1;
     } else {
         // Use Diplomacy
-        Human += 1;
+        milk += 1;
     }
 
-    if (document.getElementById("Of course").checked) {
-        Spider += 1;
-    } else if (document.getElementById("Probably not").checked) {
-        Fish += 1;
-    } else if (document.getElementById("Head empty").checked) {
-        Plankton += 1;
-        Nudibranch += 1;
+    if (document.getElementById("capitolonebank").checked) {
+        frenchfry += 1;
+    } else if (document.getElementById("chase").checked) {
+        frostedflakes += 1;
+    } else if (document.getElementById("bankofamerica").checked) {
+        milk += 1;
+        icecream += 1;
     } else {
         // Existential Crisis
-        Human += 1;
+        salad += 1;
     }
 
     resetResult();
 
-    if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Fish) {
-        document.getElementById("FishResult").style.display = "block";
-    } else if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Plankton) {
-        document.getElementById("PlanktonResult").style.display = "block";
-    } else if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Human) {
-        document.getElementById("HumanResult").style.display = "block";
-    } else if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Spider) {
-        document.getElementById("SpiderResult").style.display = "block";
-    } else if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Nudibranch) {
-        document.getElementById("NudibranchResult").style.display = "block";
+    if (Math.max(frenchfry, milk, frostedflakes, icecream, salad) === frenchfry) {
+        document.getElementById("frenchfryresult").style.display = "block";
+    } else if (Math.max(frenchfry, milk, frostedflakes, icecream, salad) === milk) {
+        document.getElementById("milkresult").style.display = "block";
+    } else if (Math.max(frenchfry, milk, frostedflakes, icecream, salad) === salad) {
+        document.getElementById("saladresult").style.display = "block";
+    } else if (Math.max(frenchfry, milk, frostedflakes, icecream, salad) === frostedflakes) {
+        document.getElementById("frostedflakesresult").style.display = "block";
+    } else if (Math.max(frenchfry, milk, frostedflakes, icecream, salad) === icecream) {
+        document.getElementById("icecreamresult").style.display = "block";
     }
 }
