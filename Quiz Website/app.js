5c52060fe031ea3612b2aabc6045f3984139c0a0
Bennett Johnson <bennettjohnson@seattleacademy.org>
2025-02-12 09:30:23 -0800

    Co-authored-by: CharlieC34 <CharlieC34@users.noreply.github.com>

--- Quiz Website/app.js
+++ Quiz Website/app.js
@@ -2,86 +2,78 @@
 'use strict';
 
 function resetResult() {
-    document.getElementById("FishResult").style.display = "none";
-    document.getElementById("PlanktonResult").style.display = "none";
-    document.getElementById("HumanResult").style.display = "none";
-    document.getElementById("SpiderResult").style.display = "none";
+    document.getElementById("SillyResult").style.display = "none";
+    document.getElementById("TacoResult").style.display = "none";
+    document.getElementById("BaldResult").style.display = "none";
+    document.getElementById("MustasheResult").style.display = "none";
 }
 function seeResult() {
-    var Fish = 0;
-    var Plankton = 0;
-    var Nudibranch = 0;
-    var Human = 0;
-    var Spider = 0;
+    var SillyBron = 0;
+    var TacoBron = 0;
+    var BaldBron = 0;
+    var MustasheBron = 0;
 
-    if (document.getElementById("8Eyes").checked) {
-        Spider += 1;
-    } else if (document.getElementById("2Eyes").checked) {
-        Human += 1;
-        Fish += 1;
-        Nudibranch += 1;
+
+    if (document.getElementById("Taco").checked) {
+        TacoBron += 1;
+        MustasheBron +=1;
+    } else if (document.getElementById("Egg").checked) {
+        BaldBron += 1;
+        
     } else {
-        // 1, 2, 3 or more depending on the species
-        Plankton += 1;
+        // Everything
+        SillyBron += 1;
     }
 
-    if (document.getElementById("Walk").checked) {
-        Human += 1;
-        Spider += 1;
-        Nudibranch += 1;
-    } else if (document.getElementById("Swim").checked) {
-        Fish += 1;
-        Plankton += 1;
-        Nudibranch += 1;
+    if (document.getElementById("TacoTuesday").checked) {
+        MustasheBron += 1;
+        TacoBron += 1;
+    } else if (document.getElementById("BoomBoomBoomBoom").checked) {
+        BaldBron += 1;
     } else {
-        // Swim upside down
-        Nudibranch += 1;
+        // ThatOurBallAintIt
+        SillyBron += 1;
     }
 
     if (document.getElementById("Yes").checked) {
-        Human += 1;
-        Spider += 1;
+        SillyBron += 1;
+        TacoBron += 1;
+        BaldBron +=1;
     } else if (document.getElementById("No").checked) {
-        Fish += 1;
-        Plankton += 1;
-        Nudibranch += 1;
+        MustasheBron += 1;
     }
 
-    if (document.getElementById("Fight").checked) {
-        Spider += 1;
-    } else if (document.getElementById("Run").checked) {
-        Fish += 1;
-        Plankton += 1;
-    } else if (document.getElementById("Secrete").checked) {
-        Nudibranch += 1;
+    if (document.getElementById("Cavs").checked) {
+        MustasheBron += 1;
+    } else if (document.getElementById("Lakers").checked) {
+        BaldBron += 1;
+    } else if (document.getElementById("Heat").checked) {
+        TacoBron += 1;
     } else {
-        // Use Diplomacy
-        Human += 1;
+        // All of them
+        SillyBron += 1;
     }
 
-    if (document.getElementById("Of course").checked) {
-        Spider += 1;
-    } else if (document.getElementById("Probably not").checked) {
-        Fish += 1;
-    } else if (document.getElementById("Head empty").checked) {
-        Plankton += 1;
-        Nudibranch += 1;
+    if (document.getElementById("LilBaby").checked) {
+        BaldBron += 1;
+    } else if (document.getElementById("TeeGrizzly").checked) {
+        TacoBron += 1;
+    } else if (document.getElementById("PopSmoke").checked) {
+        SillyBron += 1;
     } else {
-        // Existential Crisis
-        Human += 1;
+        // Other
+        MustasheBron += 1;
     }
 
     resetResult();
 
-    if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Fish) {
-        document.getElementById("FishResult").style.display = "block";
-    } else if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Plankton) {
-        document.getElementById("PlanktonResult").style.display = "block";
-    } else if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Human) {
-        document.getElementById("HumanResult").style.display = "block";
-    } else if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Spider) {
-        document.getElementById("SpiderResult").style.display = "block";
-    } else if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Nudibranch) {
-        document.getElementById("NudibranchResult").style.display = "block";
+    if (Math.max(BaldBron, SillyBron, MustasheBron, TacoBron) === BaldBron) {
+        document.getElementById("BaldResult").style.display = "block";
+    } else if (Math.max(BaldBron, SillyBron, MustasheBron, TacoBron) === SillyBron) {
+        document.getElementById("SillyResult").style.display = "block";
+    } else if (Math.max(BaldBron, SillyBron, MustasheBron, TacoBron) === MustasheBron) {
+        document.getElementById("MustasheResult").style.display = "block";
+    } else if (Math.max(BaldBron, SillyBron, MustasheBron, TacoBron) === TacoBron) {
+        document.getElementById("TacoResult").style.display = "block";
     }
 }
