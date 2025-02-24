3021966617c875819c3cba69e1dd4d333f3c3495
Lucia McNeilly <luciamcneilly@seattleacademy.org>
2025-02-24 14:46:30 -0800

    hi i did my quiz

--- Quiz Website/app.js
+++ Quiz Website/app.js
@@ -2,86 +2,68 @@
 'use strict';
 
 function resetResult() {
-    document.getElementById("FishResult").style.display = "none";
-    document.getElementById("PlanktonResult").style.display = "none";
-    document.getElementById("HumanResult").style.display = "none";
-    document.getElementById("SpiderResult").style.display = "none";
+    document.getElementById("Catresult").style.display = "none";
+    document.getElementById("Owlresult").style.display = "none";
+    document.getElementById("Jellyfishresult").style.display = "none";
 }
 function seeResult() {
-    var Fish = 0;
-    var Plankton = 0;
-    var Nudibranch = 0;
-    var Human = 0;
-    var Spider = 0;
+    var Cat = 0;
+    var Jellyfish = 0;
+    var Owl = 0;
 
-    if (document.getElementById("8Eyes").checked) {
-        Spider += 1;
-    } else if (document.getElementById("2Eyes").checked) {
-        Human += 1;
-        Fish += 1;
-        Nudibranch += 1;
-    } else {
+
+    if (document.getElementById("Quiet").checked) {
+        Cat += 1;
+    } else if (document.getElementById("Chill").checked) {
+        Owl += 1;
+    } else if (document.getElementById("Jellyfish").checked) {
         // 1, 2, 3 or more depending on the species
-        Plankton += 1;
+        Jellyfish += 1;
     }
 
-    if (document.getElementById("Walk").checked) {
-        Human += 1;
-        Spider += 1;
-        Nudibranch += 1;
-    } else if (document.getElementById("Swim").checked) {
-        Fish += 1;
-        Plankton += 1;
-        Nudibranch += 1;
-    } else {
-        // Swim upside down
-        Nudibranch += 1;
+    if (document.getElementById("Pink").checked) {
+        Cat += 1;
+    } else if (document.getElementById("Blue").checked) {
+        Jellyfish += 1;
+    } else if (document.getElementById("Neutrals").checked) {
+     Owl += 1;
+
     }
 
-    if (document.getElementById("Yes").checked) {
-        Human += 1;
-        Spider += 1;
-    } else if (document.getElementById("No").checked) {
-        Fish += 1;
-        Plankton += 1;
-        Nudibranch += 1;
+    if (document.getElementById("English").checked) {
+        Cat += 1;
+    } else if (document.getElementById("Music").checked) {
+        Owl += 1;
+    } else if (document.getElementById("School whats that").checked) {
+        Jellyfish += 1;
+
     }
 
-    if (document.getElementById("Fight").checked) {
-        Spider += 1;
-    } else if (document.getElementById("Run").checked) {
-        Fish += 1;
-        Plankton += 1;
-    } else if (document.getElementById("Secrete").checked) {
-        Nudibranch += 1;
-    } else {
-        // Use Diplomacy
-        Human += 1;
+    if (document.getElementById("HYPERPIGMENTATION").checked) {
+        Cat += 1;
+    } else if (document.getElementById("Raise your ya ya ya").checked) {
+        Owl += 1;
+    } else if (document.getElementById("Purple heart or whatever").checked) {
+        Jellyfish += 1;
+    
     }
 
-    if (document.getElementById("Of course").checked) {
-        Spider += 1;
-    } else if (document.getElementById("Probably not").checked) {
-        Fish += 1;
-    } else if (document.getElementById("Head empty").checked) {
-        Plankton += 1;
-        Nudibranch += 1;
-    } else {
-        // Existential Crisis
-        Human += 1;
+    if (document.getElementById("True Blue - Boygenius").checked) {
+        Cat += 1;
+    } else if (document.getElementById("Gypsy - Fleetwood Mac").checked) {
+        Owl += 1;
+    } else if (document.getElementById("Carey - Joni Mitchell").checked) {
+        Jellyfish += 1;
+
     }
 
     resetResult();
 
-    if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Fish) {
-        document.getElementById("FishResult").style.display = "block";
-    } else if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Plankton) {
-        document.getElementById("PlanktonResult").style.display = "block";
-    } else if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Human) {
-        document.getElementById("HumanResult").style.display = "block";
-    } else if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Spider) {
-        document.getElementById("SpiderResult").style.display = "block";
-    } else if (Math.max(Fish, Plankton, Human, Spider, Nudibranch) === Nudibranch) {
-        document.getElementById("NudibranchResult").style.display = "block";
+    if (Math.max(Cat, Owl, Jellyfish) === Cat) {
+        document.getElementById("Catresult").style.display = "block";
+    } else if (Math.max(Cat, Owl, Jellyfish) === Owl) {
+        document.getElementById("Owlresult").style.display = "block";
+    } else if (Math.max(Cat, Owl, Jellyfish) === Jellyfish) {
+
     }
 }
