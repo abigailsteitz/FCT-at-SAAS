af644c24573c0f69f0b4d703a32df96acb239e9f
kai gopal <kaigopal@seattleacemy.org>
2025-02-25 09:05:11 -0800

    Co-authored-by: FreshFMJ <FreshFMJ@users.noreply.github.com> Co-authored-by: marcusga <marcusga@users.noreply.github.com>

--- Business Website/app.js
+++ Business Website/app.js
@@ -2,18 +2,20 @@
 'use strict';
 
 function secretMessage() {
-    alert("You found the secret message! Coupon code: #Pets4Ever")
+    alert("You found the secret message! Coupon code: #Football4Ever")
 }
 function displayDog() {
+    document.body.style.backgroundColor = "Black"; 
     document.getElementById("dogContent").style.display = "block";
     document.getElementById("catContent").style.display = "none";
     document.getElementById("defaultContent").style.display = "none";
 }
 function displayCat() {
+    document.body.style.backgroundColor = "Red"; 
     document.getElementById("dogContent").style.display = "none";
     document.getElementById("catContent").style.display = "block";
     document.getElementById("defaultContent").style.display = "none";
 }
-function pinkBackground() {
-    document.body.style.backgroundColor = "pink";
+function Change() {
+    document.body.style.backgroundColor = "Green"; 
 }
