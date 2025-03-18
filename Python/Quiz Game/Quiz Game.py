9ef6fde7e46b26bb06f42a52c8f7441ac48406e9
Goldie Lackman <GoldieLackman@seattleacademy.org>
2025-03-18 12:33:23 -0700

    quiz game

--- Python/Quiz Game/Quiz Game.py
+++ Python/Quiz Game/Quiz Game.py
@@ -1,47 +1,158 @@
+print("hello world!")
 
-print ("hello world")
-print("welcome to my quiz abt chaili")
-userAnswer= input ("what is kys fav book")
-print (userAnswer)
-
-# this is conditional
-if userAnswer == "dosent have one" or  userAnswer== "ouabh" or userAnswer== "powerlsess" or userAnswer== "tog":
-  print "ya"
+print("Only 1 in 20 people can get 5/5 on this test about Flowers... Are you ready to begin?")
+print("  ")
+userAnswer = input("  ")
+if userAnswer == "No":
+  print(" ")
+  print(":(")
 else:
- print "IEFLFLIDCJNvjnnoo"
-
-userAnswer=input ("what does she survive on")
-print (userAnswer)
-
-# this is conditional
-if userAnswer == "coffee" or  userAnswer== "iv bags" or userAnswer== "the blood of her enemies" or userAnswer== "books":
-  print "yeah she needs protien"
-else:
-  print "ur wrong"
+  print(" ")
+  print("Okay lets go!")
   
-  userAnswer= input ("is kyli sane")
-print (userAnswer)
+print("please note that no capital letters will be needed to answer questions correctly")
+print("  ")
+print("lets get started:")
 
-# this is conditional
+score = 0
+
+print(" ")
+print("What type of flower do people commonly give their Significant Other?")
+print(" ")
+userAnswer = input("  ")
+
+if userAnswer == "roses":
+  print(" ")
+  print("✅ Correct! :)")
+  score += 1
+else:
+  print(" ")
+  print("❌ incorrect :(")
+  print("the correct answer was roses")  
+  
+print(" ")  
+print("What flower do people use to tell if they like butter or not?")
+print("  ")
+userAnswer = input("  ")
+
+if userAnswer == "buttercups":
+  print(" ")
+  print("✅ Correct! :)")
+  score += 1
+else:
+  print(" ")
+  print("❌incorrect :(")
+  print("the correct answer was buttercups") 
+  
+print(" ")
+print("What is the State flower of Washington?")
+print("  ")
+userAnswer = input("  ")
+
+
+if userAnswer == "rhododendron":
+  print(" ")
+  print("✅ Correct! :)")
+  score += 1
+else:
+  print(" ")
+  print("❌ incorrect :(")
+  ("the correct answer was rhododendron")
+  
+print("What flower is named for the greek goddess of the rainbow?")
+print(" ")
+print("A: Poppies")
+print("B: Daisies")
+print("C: Hydrangeas")
+print("D: Irises")
+print("  ")
+userAnswer = input(" ")
+
+
+if userAnswer == "irises":
+  print(" ")
+  print("✅ Correct! :)")
+  score += 1
+elif userAnswer == "d":
+  print(" ")
+  print("✅ Correct! :)")
+  score += 1
+elif userAnswer == "d: irises":
+  print(" ")
+  print("✅ Correct!")
+  score += 1
+elif userAnswer == "d, irises":
+  print(" ")
+  print("✅ Correct!")
+else:
+  print(" ")
+  print("❌ incorrect :(")
+  print("the correct answer was d, irises ")
+
+print(" ")
+print("What color Rose is often given as a sign of friendship?")
+print("  ")
+userAnswer = input("  ")
+
+
+if userAnswer == "yellow":
+  print(" ")
+  print("✅ Correct! :)")
+  score +=1 
+else:
+  print(" ")
+  print("❌ incorrect :(")
+  ("the correct answer was yellow")
+  
+
+print("  ")
+print("Would you like to answer a bonus challenge question?")
+print("please answer yes or no")
+print("  ")
+userAnswer = input("  ")
 if userAnswer == "no":
-  print "yeahh"
+  print(" ")
+  print("Ok, thanks for playing!")
+  print("you got " + str(score) + "/5 right!")
+  print(" ")
+  print(" ")
+  print("goodbye!")
 else:
-    print "shes not sane"
-    
-userAnswer= input ("what animal is she")
-print (userAnswer)
+  print(" ")
+  print("ok here it is:")
 
-# this is conditional
-if userAnswer == "crab" or  userAnswer== "panda" or userAnswer== "small mamal" or userAnswer== "koi fish":
-  print "yeah"
-else:
-  print (wrong)
-  
-userAnswer= input ("does she have a sivlibg")
-print (userAnswer)
+  print(" ")
+  print("In Holland, what flower used to be more valuable than gold?")
+  print(" ")
+  print("A: Daffodils")
+  print("B: Tulips")
+  print("C: Gladiolus")
+  print("D: Carnations")
+  print("  ")
+  userAnswer = input(" ")
 
-# this is conditional
-if userAnswer == "ye":
-  print "yep"
-else:
-  print "she does"
+  if userAnswer == "tulips":
+    print(" ")
+    print("✅ Correct! :)")
+    score += 1
+  elif userAnswer == "b":
+    print(" ")
+    print("✅ Correct! :)")
+    score += 1
+  elif userAnswer == "b: tulips":
+    print(" ")
+    print("✅ Correct!")
+    score += 1
+  elif userAnswer == "b, tulips":
+    print(" ")
+    print("✅ Correct!")
+    score += 1
+  else:
+    print(" ")
+    print("❌ incorrect :(")
+    ("the correct answer was tulips")
+
+  print(" ")
+  print("Thank you so much for playing")
+  print("you got " + str(score) + "/6 right!")
+  print("goodbye!")
