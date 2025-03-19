f64d9becb552256732cdc803d5876162f7c6a09e
dylanfinkel <dylanfinkelman@seattleacademy.org>
2025-03-19 14:51:50 -0700

    Update Quiz Game.py

--- Python/Quiz Game/Quiz Game.py
+++ Python/Quiz Game/Quiz Game.py
@@ -1,3 +1,93 @@
-# This file intentionally empty.
-# You should copy-paste your quiz game here!
-# Over-write this file in your own branch.
+print("Hello World!")
+
+print("Welcome to my quiz about Henry's at SAAS!")
+
+# Ask the user a question
+print("Whos the tallest Henry?")
+print("- Henry Finkelman")
+print("- Henry Kaplan")
+print("- Henry Moore")
+print("- Henry Predmore")
+# get their answer
+userAnswer = input()
+print(userAnswer)
+
+# check if it's correct
+# This is called a conditional
+if userAnswer == "Henry Kaplan":
+  print("NICE JOB BUCKO!")
+else:
+  print("WRONG, The right answer was Henry Kaplan")
+  
+# Ask the user a question
+print("Whos the shortest Henry?")
+print("- Henry Finkelman")
+print("- Henry Kaplan")
+print("- Henry Moore")
+print("- Henry Predmore")
+# get their answer
+userAnswer = input()
+print(userAnswer)
+
+# check if it's correct
+# This is called a conditional
+if userAnswer == "Henry Finkelman":
+  print("WOWZERS!")
+else:
+  print("WRONG, The right answer was Henry Finkelman")
+  
+  # Ask the user a question
+print("Whos the strongest Henry?")
+print("- Henry Finkelman")
+print("- Henry Kaplan")
+print("- Henry Moore")
+print("- Henry Predmore")
+# get their answer
+userAnswer = input()
+print(userAnswer)
+
+# check if it's correct
+# This is called a conditional
+if userAnswer == "Henry Moore":
+  print("Splendid Job Son!")
+else:
+  print("WRONG, The right answer was Henry Moore")
+  
+  # Ask the user a question
+print("Whos the Henry in 6th grade?")
+print("- Henry Finkelman")
+print("- Henry Kaplan")
+print("- Henry Moore")
+print("- Henry Predmore")
+# get their answer
+userAnswer = input()
+print(userAnswer)
+
+# check if it's correct
+# This is called a conditional
+if userAnswer == "Henry Finkelman":
+  print("WOO HOO!")
+else:
+  print("WRONG, The right asnwer was Henry Finkelman")
+  
+  # Ask the user a question
+print("Whos the craziest Henry?")
+print("- Henry Finkelman")
+print("- Henry Kaplan")
+print("- Henry Moore")
+print("- Henry Predmore")
+# get their answer
+userAnswer = input()
+print(userAnswer)
+
+# check if it's correct
+# This is called a conditional
+if userAnswer == "Henry Predmore":
+  print("BAZANGA!")
+  print("You got 50 points!")
+else:
+  print("WRONG, The right answer was Henry Predmore")
+  print("You got 50 points!")
+  
+  
+  
