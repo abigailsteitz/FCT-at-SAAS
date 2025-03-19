6fa5e816a49062e62dd30330b2f65bc652d92fe0
Henry-jit <henrymoore@seattleacademy.org>
2025-03-19 14:56:53 -0700

    I finished uploading my code to vscode

--- Python/Quiz Game/Quiz Game.py
+++ Python/Quiz Game/Quiz Game.py
@@ -1,3 +1,92 @@
 # This file intentionally empty.
 # You should copy-paste your quiz game here!
 # Over-write this file in your own branch.
+ # First day: Ask your user questions, take their answer using the
+# input() function, and tell them if they got the answer right or wrong.
+
+print("Hello World!")
+
+print("Welcome to my quiz abouy Lebron James?")
+
+# Ask the user a question
+print("how old is Lebron James")
+
+# get their answer
+userAnswer = input()
+print(userAnswer)
+
+# check if it's correct
+# This is called a conditional
+if userAnswer == "40":
+  print("YAASSS")
+else:
+  print("NOPE")
+  
+  
+print("How many NBA Championships does lebron have")
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+
+# get their answer
+userAnswer = input()
+print(userAnswer)
+
+# check if it's correct
+# This is called a conditional
+if userAnswer == "4":
+  print("Kudos to you")
+else:
+  print("Yikes buddy")
+  
+
+print("What year was lebron drafted?")
+
+# get their answer
+userAnswer = input()
+print(userAnswer)
+
+# check if it's correct
+# This is called a conditional
+if userAnswer == "2003":
+  print("Hooray")
+else:
+  print("you should be ashamed really")
+  
+print("what state did lebron play highschool basketball?")
+print("Ohio")
+print("Indiana")
+print("kentucky")
+print("California")
+print("North Carolina")
+
+# get their answer
+userAnswer = input()
+print(userAnswer)
+
+# check if it's correct
+# This is called a conditional
+if userAnswer == "Ohio" or userAnswer == "ohio"  :
+  print("exqusite")
+else:
+  print("not even close")
+  
+print("final question... this is a hard one! how many points did lebron score in his first NBA game ever")
+
+print("27")
+print("13")
+print("4")
+print("25")
+print("19")
+ 
+userAnswer = input()
+print(userAnswer)
+
+ 
+if userAnswer == "25" :
+  print("correct")
+else:
+  print("definetly not")
+  
