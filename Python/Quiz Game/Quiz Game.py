c9fabdee3fd908e65669c2f5db5f8627a6484d25
McKinley0901 <mckinleyduncan@seattleacademy.org>
2025-03-19 08:43:26 -0700

    Update Quiz Game.py
    I add my code to git hub

--- Python/Quiz Game/Quiz Game.py
+++ Python/Quiz Game/Quiz Game.py
@@ -1,3 +1,120 @@
 # This file intentionally empty.
 # You should copy-paste your quiz game here!
 # Over-write this file in your own branch.
+print("Hello World!")
+print("Today you will be quizzed on your knowledge on mostly European Countrys: Please use Capital when answering question")
+print("Question 1: what is 1 of the Countries boarders Slovakia")
+score = 0
+print("A) Poland")
+print("B) Finland")
+print("C) Netherlands")
+print("D) Belarus")
+userAnswer = input() 
+if userAnswer == "A":
+   print("correct You rock")
+   score = score + 1 
+elif userAnswer == "B":
+   print("Incorrect: the answer is Poland") 
+elif userAnswer == "C":
+   print("Incorrect: the answer is Poland") 
+elif userAnswer == "D":
+   print("Incorrect: the answer is Poland") 
+else:
+   print("Are you okay, do you need help, and the answer was Poland")
+print("Question 2: What Country has the biggest coastline")
+print("A) Russia")
+print("B) Canada")
+print("C) Norway")
+print("D) United States")
+userAnswer = input()
+if userAnswer == "B":
+  print("correct your doing great")
+  score = score + 1 
+elif userAnswer == "A":
+  print("Incorrect: the answer is Canada")
+elif userAnswer == "C":
+ print("Incorrect: the answer is Canada")
+elif userAnswer == "D":
+ print("Incorrect: the answer is Canada")
+else:
+  print("Okay now your concering me blink twice if you need help. Also the answer was Canada")
+print("Question 3: How many Countries are apart of the European Union")
+print("A)27")
+print("B)43")
+print("C)53")
+print("D)21")
+userAnswer = input()
+if userAnswer == "A":
+  print("correct you might be the smartest person I know")
+  score = score + 1 
+elif userAnswer == "B":
+  print("Incorrect: the answer is 27")
+elif userAnswer == "C":
+    print("Incorrect: the answer is 27")
+elif userAnswer == "D":
+    print("Incorrect: the answer is 27")
+else: 
+  print("why just why: the answer was 27")
+print("Question 4: What is the most visted landmark in Europe")
+print("A) Big Ben")
+print("B) Colleseum")
+print("C) Effiel Tower")
+print("D) Stonehenge ")
+userAnswer = input ()
+if userAnswer == "C":
+   print("correct ok stop showing off")
+   score = score + 1 
+elif userAnswer == "A":
+  print("Incorrect: the answer is Effiel Tower")
+elif userAnswer == "B":
+ print("Incorrect: the answer is Effiel Tower")
+elif userAnswer == "D":
+ print("Incorrect: the answer is Effiel Tower")
+else:
+  print("Please stop: By the way the answer was Effiel Tower")
+print("Finale queston yay")
+print("Question 5: what country boarders Estonia")
+print("A) Latvia")
+print("B) Finland")
+print("C) Sweden")
+print("D) Lithuania")
+userAnswer = input()
+if userAnswer == "A":
+  print("correct you show off")
+  score = score + 1 
+elif userAnswer == "B":
+  print("Incorrect: the answer is Lativa")
+elif userAnswer == "C":
+    print("Incorrect: the answer is Lativa")
+elif userAnswer == "D":
+    print("Incorrect: the answer is latvia")
+else: 
+ print("your sad very sad")
+print("congrats you did it, you might not have gotten every answer correct but you still did it")
+print("now just for you lets give you 1 last question")
+print("Bouns Question: How many characters does the country with the longest name have")
+print("A)27")
+print("B)43")
+print("C)56")
+print("D)92")
+userAnswer = input()
+if userAnswer == "C":
+  print("correct you have a IQ of 164")
+  score = score + 1 
+elif userAnswer == "B":
+  print("Incorrect: the answer is 56")
+elif userAnswer == "A":
+    print("Incorrect: the answer is 56")
+elif userAnswer == "D":
+    print("Incorrect: the answer is 56")
+else: 
+ print("i hope your happy with your self: the Answer was 56.")
+print("The name is: The United Kingdom of Great Britain and Northern Ireland")
+
+if userAnswer == "C":
+  print("Congrats for finishing my quiz I hope you had fun.")
+print ("You got " + str(score) + " out of 6 correct!")
+
+
+    
+   
