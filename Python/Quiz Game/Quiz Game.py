16aa32f3ad5c52f72914f334970f79cd033851de
Chris-to-the-pher <christopherwinkler@seattleacademy.org>
2025-03-19 14:39:23 -0700

    I crave burgers

--- Python/Quiz Game/Quiz Game.py
+++ Python/Quiz Game/Quiz Game.py
@@ -1,3 +1,60 @@
-# This file intentionally empty.
-# You should copy-paste your quiz game here!
-# Over-write this file in your own branch.
+#wood fired pizza... what will pizza do now?
+print("Hello Cruel World")
+print("heres a quiz")
+print("first question...")
+#asking a question
+score = 0
+print("what is Lily's name?")
+userAnswer = input()
+#print(userAnswer)
+if userAnswer == "Lily" or userAnswer == "lily":
+  print("Excellent")
+  score = score + 1
+else:
+  print("bruh I just said it")
+print("What is Lily's favorite color?")
+UserAnswer = input()
+#print(UserAnswer)
+if UserAnswer == "red" or UserAnswer == "Red" or UserAnswer == "ur mom":
+  print("Indeed")
+  score = score + 1
+else:
+  print("nope")
+
+print("What sport does Lily play?")
+useranswer1 = input()
+#print(useranswer1)
+if useranswer1 == "soccer" or useranswer1 == "Soccer":
+  print("yup")
+  score = score + 1
+else:
+  print("no")
+
+#if you drop soap on the ground is the floor clean or is the soap dirty? well the floor is still dirty because you need to wash the soap off the floor so the soap is now dirty
+
+print("What IS lily?")
+print("a) a human")
+print("b) a horse with 5 legs")
+useranswer2 = input()
+#print(useranswer2)
+if useranswer2 == "a" or useranswer2 == "A" or useranswer2 == "human" or useranswer2 == "a human":
+  print("that's what you think")
+if useranswer2 == "b" or useranswer2 == "B" or useranswer2 == "a horse with 5 legs":
+  print("absolutly")
+  score = score + 1
+else:
+  print("incorrect")
+
+print("What is Lily's favorite animal?")
+useranswer3 = input()
+#print(useranswer3)
+if useranswer3 == "cat" or useranswer3 == "Cat":
+  print("correct") 
+  score = score + 1
+else:
+  print("incorrect")
+  
+print("You got " + str(score) + " out of 5 correct")
+print("aight bye")
+
+#hello (:
