b6fd2a9d4f412f7b9790e07c3ecaa6ce334f266f
candyland88888 <dylanterasaki@seattleacademy.org>
2025-03-20 09:37:52 -0700

    Update QuizGame

---
+++ Python/Quiz Game/Quiz Game.py
@@ -0,0 +1,73 @@
+#. Goal take their answer using the input() function, and tell them if they got the answer right or wrong.
+print ("hello world")
+
+print ("welcome to my quiz about tim")
+score = 0
+
+userANswer = input("what is tims birthday")
+print(userANswer)
+
+if userANswer == "aug 10" or userANswer == "August 10" or userANswer == "8/10" or userANswer == "8-10":
+  print ("YAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+  score = score + 2.3123
+  print("You got " + str(score) + " out of five correct")
+
+else:
+  print ("booooooooooooooooooooooooo")
+  
+useranswer = input("what is tims fave form of tranportation?")
+print (useranswer)
+
+if useranswer == "train":
+  print ("ya")
+  score += 3.4843
+  print("You got " + str(score) + " out of five correct")
+
+else: 
+  print ("no")
+  
+useranSwer = input("what color is tims hair?")
+print (useranSwer)
+
+if useranSwer == "red" or useranSwer == "ginger" or useranSwer == "orange":
+  print ("ye")
+  score += 1.23563
+  print("You got " + str(score) + " out of five correct")
+  
+else: 
+  print("nope youre wrong")
+
+  
+useransWer = input("what is tims dogs name?")
+print (useransWer)
+
+if useransWer == "biscuit" or useransWer == "splash":
+  print ("yeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+  score += 9.3473422
+  print("You got " + str(score) + " out of five correct")
+
+else: 
+  print ("nooo")
+  
+usEranswer = input("does him have a sibling")
+print (usEranswer)
+
+if usEranswer == "yes":
+  print ("oui")
+  score += 2832.29349239433042349839429384923749832748
+  print("You got " + str(score) + " out of five correct")
+
+else: 
+  print ("no")
+ 
+print( )
+print("your score is " + str(score)+ " out of 5")
+print( )
+print( )
+print( )
+print( ) 
+print("i like penguins")
+  
+  
+  
+  
