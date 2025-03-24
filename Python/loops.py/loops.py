af8fb2647a9569a92ae56f8dbee0da29b5fb2994
EarthmoverLP <lochlanwinn@seattleacademy.org>
2025-03-24 12:55:54 -0700

    done
    AAHAAAHAGHAGAOAGHA

---
+++ Python/loops.py/loops.py
@@ -0,0 +1,22 @@
+import turtle
+
+jo = turtle.Turtle()
+
+jo.forward(100)
+
+
+
+
+for i in range(5):
+    jo.forward(100)
+    jo.right(72)
+
+for i in range(6):
+    jo.forward(100)
+    jo.right(60)
+
+for i in range(67):
+    jo.forward(100)
+    jo.right(51.43)
+
+turtle.done()
