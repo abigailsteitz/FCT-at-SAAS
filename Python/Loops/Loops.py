e548e3c9f48ed11413b148eeb5cb0dc6035f773f
McKinley0901 <mckinleyduncan@seattleacademy.org>
2025-03-25 09:27:43 -0700

    Lupa made sad loops

---
+++ Python/Loops/Loops.py
@@ -0,0 +1,64 @@
+import turtle
+Lupa = turtle.Turtle()
+Lupa.speed(10)
+for i in range(8):
+    Lupa.forward(100)
+    Lupa.right(45)
+for i in range(3):
+    Lupa.forward(100)
+    Lupa.right(120)
+Lupa.forward(100)
+Lupa.right(45)
+for i in range(3):
+    Lupa.forward(100)
+    Lupa.right(120)
+Lupa.forward(100)
+Lupa.right(45)
+for i in range(3):
+    Lupa.forward(100)
+    Lupa.right(120)
+Lupa.forward(100)
+Lupa.right(45)
+for i in range(3):
+    Lupa.forward(100)
+    Lupa.right(120)
+Lupa.forward(100)
+Lupa.right(45)
+for i in range(3):
+    Lupa.forward(100)
+    Lupa.right(120)
+Lupa.forward(100)
+Lupa.right(45)
+for i in range(3):
+    Lupa.forward(100)
+    Lupa.right(120)
+Lupa.forward(100)
+Lupa.right(45)
+for i in range(3):
+    Lupa.forward(100)
+    Lupa.right(120)
+Lupa.forward(100)
+Lupa.right(45)
+for i in range(3):
+    Lupa.forward(100)
+    Lupa.right(120)
+Lupa.forward(100)
+Lupa.right(45)
+Lupa.left(90)
+Lupa.forward(100)
+for i in range(13):
+    Lupa.forward(100)
+    Lupa.right(30)
+Lupa.left(90)
+Lupa.forward(100)
+for i in range(3):
+    Lupa.forward(100)
+    Lupa.right(120)
+for i in range(3):
+    Lupa.left(60)
+    Lupa.forward(100)
+    Lupa.right(120)
+    Lupa.forward(100)
+    Lupa.right(120)
+    
+turtle.done()
