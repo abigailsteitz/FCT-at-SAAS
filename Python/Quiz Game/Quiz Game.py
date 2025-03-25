7c86d3a97427d08e8e5a5464f22d9f571020ff06
gavinnolzthegoat <gavinnolz@seattleacademy.org>
2025-03-25 09:13:39 -0700

    turning in my loops assignment

--- Python/Quiz Game/Quiz Game.py
+++ Python/Quiz Game/Quiz Game.py
@@ -1,3 +1,98 @@
 # This file intentionally empty.
 # You should copy-paste your quiz game here!
 # Over-write this file in your own branch.
+print("Hello World!")
+print("Welcome to my quiz about pop culture")
+
+print("Q1: Who is the catch me outside girl?")
+print("a) Bhad Bhabie")
+print("b) Alabama Barker")
+print("c) Woah Vicky")
+print("d) Lil Tay")
+
+# this is a variable that holds the user's input
+userAnswer = input()
+print(userAnswer)
+
+# Tell the user if they got it correct
+# This is called a conditional
+if userAnswer == "a" or userAnswer == "A":
+  print("Period")
+elif userAnswer == "b":
+  print("Wrong quit now")
+elif userAnswer == "c":
+  print("You suck babes")
+elif userAnswer == "d":
+  print("Ewwwww")
+
+print("Q2: Who said boyfriend im nervous")
+print("a) Charli damelio")
+print("b) Addison rae")
+print("c) Nessa barret")
+print("d) Dixe damelio")
+
+userAnswer = input()
+print(userAnswer)
+
+if userAnswer == "b" or userAnswer == "B":
+  print("Yass")
+elif userAnswer == "a":
+  print("deaduzz?")
+elif userAnswer == "c":
+  print("You suck babes")
+elif userAnswer == "d":
+  print("Ewwwww")
+  
+print("Q3: who created floptropica")
+print("a) jifaei")
+print("b) nicki manaj")
+print("c) what i do girl")
+print("d) Gavin")
+
+userAnswer = input()
+print(userAnswer)
+
+if userAnswer == "a" or userAnswer == "A":
+  print("period")
+elif userAnswer == "b":
+  print("no dumby foo.")
+elif userAnswer == "c":
+  print("You suck babes")
+elif userAnswer == "d":
+  print("Ewwwww")
+  
+print("Q4: whos the most cancled on youtube")
+print("a) trisha paytas")
+print("b) tana mougeu")
+print("c) collenge ballinger")
+print("d) james charles")
+
+userAnswer = input()
+print(userAnswer)
+
+if userAnswer == "d" or userAnswer == "D":
+  print("wow u so good")
+elif userAnswer == "a":
+  print("no shes uncancleable.")
+elif userAnswer == "c":
+  print("You suck babes")
+elif userAnswer == "b":
+  print("last")
+
+print("Q5: whats the most used app")
+print("a) instagram")
+print("b) snapchat")
+print("c) tiktok")
+print("d) facebook")
+
+userAnswer = input()
+print(userAnswer)
+
+if userAnswer == "c" or userAnswer == "C":
+  print("wow girl do ur works")
+elif userAnswer == "a":
+  print("no girl")
+elif userAnswer == "d":
+  print("are u 45")
+elif userAnswer == "b":
+  print("wow ur unstoppable....")
