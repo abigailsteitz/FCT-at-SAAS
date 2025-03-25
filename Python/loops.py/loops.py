909f6c28dc010fb6598fe4c02d806e925a375937
BarrettMilne <barrettmilne@seattleacademy.org>
2025-03-25 09:23:49 -0700

    loops.py

---
+++ Python/loops.py/loops.py
@@ -0,0 +1,21 @@
+import turtle
+
+jo = turtle.Turtle()
+
+jo.forward(100)
+
+
+for i in range(12):
+    jo.forward(90)
+    jo.left(213)
+
+
+
+for i in range(4):
+    jo.forward(120)
+    jo.left(120)
+
+
+for i in range(12):
+    jo.forward(200)
+    jo.left(30)
