900964cd1ef3d8ade1b575ed6b98fd2f2211c37d
aholtzen <Aliceholtzen@seattleacademy.org>
2025-03-31 14:41:59 -0700

    practicing sum, largest, smallest, and average

---
+++ Python/lists/lists.py
@@ -0,0 +1,40 @@
+file = open('python/lists/data.csv','r')
+data = file.read().splitlines()
+
+print(data)
+
+for i in range(len(data)):
+    data[i] = float(data[i])
+
+
+print(data)
+
+SmallestSoFar = data[0]
+for element in data:
+    print(element)
+    if element < SmallestSoFar:
+        SmallestSoFar = element
+
+
+print("Smallest:")
+print(SmallestSoFar)
+
+SumSoFar = 0
+for element in data:
+        SumSoFar += element
+
+print("Sum:")
+print(SumSoFar)
+
+LargestSoFar = data[0]
+for element in data:
+     print(element)
+     if element > LargestSoFar:
+        LargestSoFar = element
+
+print("Largest:")
+print(LargestSoFar)
+
+Average = SumSoFar / len(data)
+print("Average:")
+print(Average)
