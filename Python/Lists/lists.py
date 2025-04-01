d038275531155e29a043b1f7ece0884a263f391b
Grayson-4 <graysonniccolls@seattleacademy.org>
2025-04-01 10:46:43 -0700

    house drawing

---
+++ Python/Lists/lists.py
@@ -0,0 +1,34 @@
+file = open("Python/Lists/data.csv")
+data = file.read().splitlines()
+print(data)
+
+#make every value in the list an integer
+for i in range(len(data)):
+    data[i] = float(data[i])
+
+print(data)
+
+#find the smallest 
+smallestSoFar = data[0]
+for element in data:
+    print("evaluating", element)
+    if element < smallestSoFar:
+        smallestSoFar = element
+print(smallestSoFar)
+#find the sum 
+total = 0
+for element in data:
+    total += element
+
+print ("the sum is",total)
+
+#find the average
+average = total / len(data)
+print("the average is", average)
+
+#find the largest
+largestSoFar = data[0]
+for element in data:
+    print("evaluating", element)
+    if element > largestSoFar:
+        largestSoFar = element
