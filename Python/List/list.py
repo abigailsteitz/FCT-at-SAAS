1efca20a72243fdfedee14179c8ffc5a08bc10f2
FletcherHume <fletcherhume@seattleacademy.org>
2025-04-01 10:36:40 -0700

    Here is my data list stuff.

---
+++ Python/List/list.py
@@ -0,0 +1,36 @@
+file = open ("python/List/data.csv")
+data = file.read().splitlines()
+
+print(data)
+
+for i in range (len(data)):
+    data[i] = float(data[i])
+
+print(data)
+
+smallestSoFar = data[0]
+for element in data:
+    if element < smallestSoFar:
+        smallestSoFar = element
+print("the smallest number is")
+print(smallestSoFar)
+
+largestSoFar = data[0]
+for element in data:
+    if element > largestSoFar:
+        largestSoFar = element
+print("the largest number is")
+print(largestSoFar)
+
+#find the sum
+
+allnumbsadded = 0
+for element in data:
+    allnumbsadded += element
+print("the sum is")
+print(allnumbsadded)
+
+#find the average
+average = allnumbsadded / len(data)
+print("the average is")
+print(average)
