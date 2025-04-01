100acd384bdc9d2c64ff6f8864406d54a1716093
paigeweiskopf <paigeweiskopf@seattleacademy.org>
2025-04-01 10:49:57 -0700

    Lists

---
+++ Python/Lists/lists.py
@@ -0,0 +1,34 @@
+file = open("Python/Lists/data.csv")
+data = file.read().splitlines()
+
+print(data)
+
+for i in range(len(data)):
+    data[i] = float(data[i])
+
+print(data)
+
+# Find the smallest
+smallestSoFar = data[0]
+for element in data:
+    if element < smallestSoFar:
+        smallestSoFar = element
+print("The smallest number is:")
+print(smallestSoFar)
+
+# Find the sum
+allnumbersadded=0
+for element in data:
+    allnumbersadded += element
+print("The sum is:")
+print(allnumbersadded)
+
+# Find the average
+average = allnumbersadded / len(data)
+print("The average is:")
+print(average)
+
+# Find the median
+sortedData = sorted(data)
+print("The sorted data is:")
+print(sortedData)
