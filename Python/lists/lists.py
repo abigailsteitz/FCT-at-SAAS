de26b0b8ecf47ff38bbd9abb4f807605867ea0e1
laure-cntr <laurendesmons@seattleacademy.org>
2025-04-01 10:40:56 -0700

    i found the average medium and sum
    lists

---
+++ Python/lists/lists.py
@@ -0,0 +1,36 @@
+file = open("Python/lists/data.csv")
+data = file.read().splitlines()
+
+print(data)
+
+#make every value a floating point number(decimal)
+for i in range(len(data)):
+    data[i] = float(data[i])
+
+print(data)
+
+#find the smallest
+smallestsofar = data[0]
+for element in data:
+    if element < smallestsofar:
+        smallestsofar = element
+
+print(smallestsofar)
+#find the sum
+allnumsadded=0
+for element in data:
+    allnumsadded += element
+print("the sum is:")
+print(allnumsadded)
+
+# find the medium 
+data.sort()
+if len(data) % 2 == 0:
+    medium = (data[len(data)//2] + data[len(data)//2 - 1]) / 2
+else:
+    medium = data[len(data)//2]
+print("the medium is:")
+print(medium)   
+# find the average
+average = allnumsadded / len(data)
+print("the average is:")
