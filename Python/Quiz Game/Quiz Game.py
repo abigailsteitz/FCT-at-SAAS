f87be7a8ef8bae55b32499d8c0c83a84e63c9a24
Noskeelmo <Ezrahausmann@seattleacademy.org>
2025-04-02 13:46:15 -0700

    Update Quiz Game.py

--- Python/Quiz Game/Quiz Game.py
+++ Python/Quiz Game/Quiz Game.py
@@ -1,3 +1,110 @@
-# This file intentionally empty.
-# You should copy-paste your quiz game here!
-# Over-write this file in your own branch.
+# First day: Ask your user questions,
+# take their answer using the input() function,
+# and tell them if they got the answer right or wrong.
+print("Hello World!")
+print("Welcome to the quiz about Lebron Raymone James Sr.")
+
+print("Q1: What is Lebron's birthday?")
+print("a) December 30, 1984")
+print("b) October 8, 1987")
+
+# this is a variable that holds the user's input
+userAnswer = input()
+print(userAnswer)
+
+# Tell the user if they got it correct
+# This is called a conditional
+if userAnswer == "a" or userAnswer == "A":
+  print("NOICE")
+elif userAnswer == "b":
+  print("Your bum ass got the question wrong")
+else:
+  print("What did you type???")
+# First day: Ask your user questions,
+# take their answer using the input() function,
+# and tell them if they got the answer right or wrong.
+print("Hello World!")
+print("Welcome to my quiz about Lebron Raymone James Sr.")
+
+print("Q1: Is Lebron the GOAT?")
+print("a) Yes")
+print("b) No")
+
+# this is a variable that holds the user's input
+userAnswer = input()
+print(userAnswer)
+
+# Tell the user if they got it correct
+# This is called a conditional
+if userAnswer == "a" or userAnswer == "A":
+  print("NOICE")
+elif userAnswer == "b":
+  print("Wrong Buster")
+else:
+  print("What did you type???")
+  # First day: Ask your user questions,
+# take their answer using the input() function,
+# and tell them if they got the answer right or wrong.
+print("Hello World!")
+print("Welcome to my quiz about Lebron Raymone James Sr.")
+
+print("Q1: What is Lebron Your Sunshine?")
+print("a) Yes")
+print("b) No")
+
+# this is a variable that holds the user's input
+userAnswer = input()
+print(userAnswer)
+
+# Tell the user if they got it correct
+# This is called a conditional
+if userAnswer == "a" or userAnswer == "A":
+  print("NOICE")
+elif userAnswer == "b":
+  print("You suck")
+else:
+  print("What did you type???")
+  # First day: Ask your user questions,
+# take their answer using the input() function,
+# and tell them if they got the answer right or wrong.
+print("Hello World!")
+print("Welcome to my quiz about my Lebron Raymone James Sr")
+
+print("Q1: Who is the best player Lebron has ever played with?")
+print("a) Norris Cole")
+print("b) Dwayne Wade")
+
+# this is a variable that holds the user's input
+userAnswer = input()
+print(userAnswer)
+
+# Tell the user if they got it correct
+# This is called a conditional
+if userAnswer == "a" or userAnswer == "A":
+  print("NOICE")
+elif userAnswer == "b":
+  print("Wrong Answer Bucko")
+else:
+  print("What did you type???")
+  # First day: Ask your user questions,
+# take their answer using the input() function,
+# and tell them if they got the answer right or wrong.
+print("Hello World!")
+print("Welcome to my quiz about Lebron Raymone James Sr")
+
+print("Q1: Do you like Lebron?")
+print("a) Yes")
+print("b) No")
+
+# this is a variable that holds the user's input
+userAnswer = input()
+print(userAnswer)
+
+# Tell the user if they got it correct
+# This is called a conditional
+if userAnswer == "a" or userAnswer == "A":
+  print("NOICE")
+elif userAnswer == "b":
+  print("Yes you do")
+else:
+  print("What did you type???")
