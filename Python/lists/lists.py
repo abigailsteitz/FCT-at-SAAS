595c36f50683c460d94042e587605aa08181251f
riley13smitty <rileysmith@seattleacademy.org>
2025-04-03 11:57:52 -0700

    Update lists.py

--- Python/lists/lists.py
+++ Python/lists/lists.py
@@ -17,9 +17,14 @@ for element in data:
 print("The smallest number is", element)
 
 
-
-
 allnumsadded=0
 for element in data:
     allnumsadded += element
 print("The sum of the numbers is", allnumsadded)
+
+average = sum(data) / len(data)  # Divide the total sum by the number of elements
+print("The average of the numbers is", average) 
+#also can use
+# find the sum of the list of numbers 
+#total_sum = sum(data)  # Use the sum() function to calculate the total
+#print("The sum of the numbers is", total_sum)
