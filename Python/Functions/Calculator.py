f54bd014f577f03307f302773fc3cda2c1a62267
indybruh <indyhuang@seattleacademy.org>
2025-04-04 08:42:20 -0700

    apr 4 assignment  functions

--- Python/Functions/Calculator.py
+++ Python/Functions/Calculator.py
@@ -7,7 +7,16 @@ print("Welcome to the calculator!")
 # TODO: Task 2, same but subtraction
 # TODO: Task 3, same but multiplication
 # TODO: Task 4, same but division
-
+def add_numbers(num1, num2):
+    return num1 + num2
+def subtract_numbers(num1, num2):
+    return num1 - num2
+def multiply_numbers(num1, num2):
+    return num1 * num2
+def divide_numbers(num1, num2):
+    if num2 == 0:
+        return "divide by zero error."
+    else: return num1 / num2
 playing = True
 while playing:
     print("Please choose an operation:")
@@ -20,15 +29,31 @@ while playing:
 
     if userAnswer == "a":
         # Replace pass with getting user input and calling your function
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        result = add_numbers(num1, num2)
+        print(f"The result is: {result}")
         pass
     elif userAnswer == "b":
         # Replace pass with getting user input and calling your function
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        result = subtract_numbers(num1, num2)
+        print(f"The result is: {result}")
         pass
     elif userAnswer == "c":
         # Replace pass with getting user input and calling your function
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        result = multiply_numbers(num1, num2)
+        print(f"The result is: {result}")
         pass
     elif userAnswer == "d":
         # Replace pass with getting user input and calling your function
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        result = divide_numbers(num1, num2)
+        print(f"The result is: {result}")
         pass
     elif userAnswer == "e":
         print("Thanks for using the calculator!")
