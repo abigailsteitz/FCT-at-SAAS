d44a0afadb28fa2124403fa4a0b1d1e91807b374
EarthmoverLP <lochlanwinn@seattleacademy.org>
2025-04-09 12:49:36 -0700

    ijgfjihkojp[poiulyktjhregnrjmty,kul.i/;o'
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa

--- Python/loops.py/Untitled-1.py
+++ Python/loops.py/Untitled-1.py
@@ -22,14 +22,37 @@ for i in range(5):
     lebronjames.left(90)
 
 lebronjames.penup()
-lebronjames.goto(150, 125)
+lebronjames.goto(260, 250)
 lebronjames.pendown()
 
 # draw concetric squares
-for banana in range(5):
-   for i in range (6):
+for banana in range(25):
+    for i in range (4):
         lebronjames.forward(10*banana)
         lebronjames.left(90)
+    lebronjames.penup()
+    lebronjames.goto(lebronjames.xcor() -5,lebronjames.ycor()-5)
+    lebronjames.pendown()
+
+#draw spiral triangle
+lebronjames.penup()
+lebronjames.goto(260, 400)
+lebronjames.pendown()
+
+lebronjames.setheading(180)
+
+for i in range(20):
+   
+    lebronjames.right(120) 
+    lebronjames.forward(10*i) 
+
+lebronjames.penup()
+lebronjames.goto(50, 50)
+lebronjames.pendown()
+        
+        
+
+
 
 
     
