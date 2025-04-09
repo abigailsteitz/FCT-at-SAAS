d96baf046e5ac37f24c9c304196e8dd380101085
Elle Hunt <ellehunt@seattleacademy.org>
2025-04-09 11:28:35 -0700

    Update lists.py

--- Python/Lists/lists.py
+++ Python/Lists/lists.py
@@ -1,5 +1,5 @@
 # code goes here
-file = open("Python/Lists/data.csv",)
+file = open("Python/Lists/data.csv")
 data = file.read().splitlines()
 
 print(data)
@@ -15,11 +15,24 @@ smallestSoFar = data [0]
 for element in data:
     if element < smallestSoFar:
         smallestSoFar = element
+print("this is the smallest number")
 print(smallestSoFar)
 
+# Find the largest
+largestSoFar = data [0]
+for element in data:
+    if element > largestSoFar:
+        largestSoFar = element
+print("this is the largest number")
+print(largestSoFar)
 # Find the sum
 allnumsadded=0
 for element in data:
     allnumsadded += element
 print("The sum is:")
 print(allnumsadded)
+# What is an average
+# An average is the sum of all the numbers divided by how many numbers there are
+average = allnumsadded / (len(data))
+print("The average is:")
+print(allnumsadded/ (len(data)))
