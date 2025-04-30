fea1829688a42516f8d6f0a3014e47f0c5e5a1b9
candyland88888 <dylanterasaki@seattleacademy.org>
2025-04-29 18:41:53 -0700

    Calculator updated

--- Python/Functions/Calculator.py
+++ Python/Functions/Calculator.py
@@ -8,6 +8,26 @@ print("Welcome to the calculator!")
 # TODO: Task 3, same but multiplication
 # TODO: Task 4, same but division
 
+# Function for addition
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Function for subtraction
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
+# Function for multiplication
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+# Function for division
+def divide_numbers(num1, num2):
+    if num2 == 0:
+        return "Error: Division by zero is not allowed."
+    return num1 / num2
+
+print("Welcome to the calculator!")
+
 playing = True
 while playing:
     print("Please choose an operation:")
@@ -19,36 +39,32 @@ while playing:
     userAnswer = input()
 
     if userAnswer == "a":
-        # Replace pass with getting user input and calling your function
-        useranswer = input("Enter two numbers separated by a space: ")
-        numbers = useranswer.split()
+        userAnswer = input("Enter two numbers separated by a space: ")
+        numbers = userAnswer.split()
         num1 = float(numbers[0])
         num2 = float(numbers[1])
-        print(num1 + num2)
-
+        print(add_numbers(num1, num2))
 
     elif userAnswer == "b":
         userAnswer = input("Enter two numbers separated by a space: ")
         numbers = userAnswer.split()
         num1 = float(numbers[0])
         num2 = float(numbers[1])
-        print(num1 - num2)
-
+        print(subtract_numbers(num1, num2))
 
     elif userAnswer == "c":
         userAnswer = input("Enter two numbers separated by a space: ")
         numbers = userAnswer.split()
         num1 = float(numbers[0])
         num2 = float(numbers[1])
-        print(num1 * num2)
+        print(multiply_numbers(num1, num2))
 
     elif userAnswer == "d":
         userAnswer = input("Enter two numbers separated by a space: ")
         numbers = userAnswer.split()
         num1 = float(numbers[0])
         num2 = float(numbers[1])
-        print(num1 / num2)
-
+        print(divide_numbers(num1, num2))
 
     elif userAnswer == "e":
         print("Thanks for using the calculator!")
