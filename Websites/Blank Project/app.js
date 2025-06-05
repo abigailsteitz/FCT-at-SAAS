40758f6a5f7494149df1184685752c062e693b13
Elslaine16 <Elsakeyes@seattleacademy.org>
2025-06-05 08:58:48 -0700

    Confusing things I need to work out later

--- Websites/Blank Project/app.js
+++ Websites/Blank Project/app.js
@@ -1,36 +1,47 @@
 // Switch to strict mode to get more useful errors when you make mistakes.
 'use strict';
 
+/* Gets the cart object from browser storage */
+function getCart() {
+    const storedCart = sessionStorage.getItem('cart');
+    return storedCart ? JSON.parse(storedCart) : [];
+}
 
-var cart = [
+/* Adds an item to the cart in browser storage */
+function addItem(item) {
+    var cart = getCart();
+    cart.push(item);
+    sessionStorage.setItem('cart', JSON.stringify(cart));
+}
 
-];
-
-/* Your JavaScript code goes here */
+/* HTML pages should call this function to add an item to the cart */
 function addToCart(item) {
 
     // Pops up a dismissable window
-    cart.push(item);
+    addItem(item);
     alert(item + " added to your cart.");
 }
 
+/* When the cart page loads, adds items to HTML */
 document.addEventListener("DOMContentLoaded", function () {
     if (window.location.pathname.endsWith("cart.html")) {
         loadCart();
     }
 });
+
+function buy() {
+    alert("Thank you for your purchase!");
+}
+
 function loadCart() {
     console.log("There are " + cart.length + " items in your cart.");
-    for (item in cart) {
-        console.log("item in cart: " + cart[item])
+    for (var item of getCart()) {
+        console.log("item in cart: " + item)
         var listItem = document.createElement("li");
-        listItem.textContent = cart[item];
+        listItem.textContent = item;
         document.getElementById("cart").appendChild(listItem);
     }
 
     console.log("loading cart");
 }
 
-function buy() {
-    alert("Thank you for your purchase!");
-}
